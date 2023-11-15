77373f64dc0a77021bb220888cf7492762405448
Diego Paiva <diegopaivaesilva@gmail.com>
2023-11-15 16:02:42 -0300

    Update `tailwind.config.js`

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,30 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',  
-  purge: [
-      "./themes/**/layouts/**/*.html",
-      "./content/**/layouts/**/*.html",
-      "./layouts/**/*.html",
-      "./content/**/*.html",
-      "./content/**/*.md",
-      "./public/**/*.html",
-    ],
-    content: [
-      "./themes/**/layouts/**/*.html",
-      "./content/**/layouts/**/*.html",
-      "./layouts/**/*.html",
-      "./content/**/*.html",
-      "./content/**/*.md",
-      "./public/**/*.html",
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          'sans' : ['"Inter"', '-apple-system' , 'BlinkMacSystemFont', 'avenir next', 'avenir', 'segoe ui', 'helvetica neue', 'helvetica', 'Cantarell', 'Ubuntu', 'roboto', 'noto', 'arial', 'sans-serif'],
-        },
+  important: true,
+  darkMode: 'class',
+  content: [
+    "./themes/**/layouts/**/*.html",
+    "./content/**/layouts/**/*.html",
+    "./layouts/**/*.html",
+    "./content/**/*.html",
+    "./content/**/*.md",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['"Inter"', '-apple-system', 'BlinkMacSystemFont', 'avenir next', 'avenir', 'segoe ui', 'helvetica neue', 'helvetica', 'Cantarell', 'Ubuntu', 'roboto', 'noto', 'arial', 'sans-serif'],
       },
     },
-    plugins: [],
-    variants: ['group-hover'],
-  }
-  
+  },
+  plugins: [],
+  variants: ['group-hover'],
+}
+
