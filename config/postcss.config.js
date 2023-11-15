8f0e7a9bf6e541930d31f547f3520f439bc210ea
Diego Paiva <diegopaivaesilva@gmail.com>
2023-11-15 15:29:03 -0300

    Update dependencies and optimize CSS with cssnano

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,18 +1,21 @@
-const themeDir = __dirname + '/../';
+const themeDir = __dirname + "/../";
 
-const purgecss = require("@fullhuman/postcss-purgecss")({
-    content: ["./hugo_stats.json"],
-    defaultExtractor: (content) => {
-      const els = JSON.parse(content).htmlElements;
-      return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
+const cssnanoPlugin = require("cssnano")({
+  path: [themeDir],
+  preset: [
+    "default",
+    {
+      discardComments: {
+        removeAll: true,
+      },
     },
-    safelist: [],
-  });
+  ],
+});
 
 module.exports = {
-    plugins: [
-        require('tailwindcss')(themeDir + './config/tailwind.config.js'),
-        require('autoprefixer')({ path: [themeDir] })
-    ]
-}
-
+  plugins: [
+    require("tailwindcss")(themeDir + "./config/tailwind.config.js"),
+    require("autoprefixer")({ path: [themeDir] }),
+    ...(process.env.HUGO_ENVIRONMENT === "production" ? [cssnanoPlugin] : []),
+  ],
+};
